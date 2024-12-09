5b3f7c19c995bd63ad3f4713262b3ff7a241be68
dejosemario <josemariaodumodu@gmail.com>
2024-12-09 13:57:20 +0100

    feat: create needed modules for auth

--- node-api/src/app.module.ts
+++ node-api/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  ConfigModule.forRoot(),
-  imports: [PrismaModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
