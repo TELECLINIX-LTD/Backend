7cb0052b6ffa5c3d3cc77d76e8886767628c685f
dejosemario <josemariaodumodu@gmail.com>
2025-01-06 23:32:57 +0100

    fix: sort out complete-profile error

--- node-api/src/patient/patient.service.ts
+++ node-api/src/patient/patient.service.ts
@@ -10,31 +10,36 @@ export class PatientService {
     private readonly emailService: EmailService,
   ) {}
   async completeProfile(
+    userId: string,
     patientProfileDto: PatientProfileDto,
-    userId: any,
   ): Promise<any> {
-    const { dateOfBirth, bloodGroup, height, weight } = patientProfileDto;
-    //find if the patient exists
-    const patient = await this.db.patient.findUnique({
-      where: { userId: userId },
-    });
-    if (!patient) {
-      throw new Error('Patient not found');
-    }
-    //update the patient profile
-    await this.db.patient.update({
-      where: { id: patient.id },
-      data: {
-        dateOfBirth: dateOfBirth,
-        bloodGroup: bloodGroup,
-        height: height,
-        weight: weight,
-      },
-    });
+    try {
+      const { dateOfBirth, bloodGroup, height, weight } = patientProfileDto;
+      //find if the patient exists
+      const patient = await this.db.patient.findUnique({
+        where: { userId: userId },
+      });
+      if (!patient) {
+        throw new Error('Patient not found');
+      }
+      //update the patient profile
+      await this.db.patient.update({
+        where: { id: patient.id },
+        data: {
+          dateOfBirth: dateOfBirth,
+          bloodGroup: bloodGroup,
+          height: height,
+          weight: weight,
+        },
+      });
 
-    return {
-      message: 'Patient profile updated successfully',
-    };
+      return {
+        message: 'Patient profile updated successfully',
+      };
+    } catch (error) {
+      console.error('Error updating patient profile:', error);
+      throw new Error('An error occurred while updating the profile.');
+    }
   }
 
   async getAllVerifiedPatients() {
