77b825420aadb84b543af513143bbaf01ea4f79b
dejosemario <josemariaodumodu@gmail.com>
2025-01-09 18:06:45 +0100

    fix: remove confirmPassword props

--- node-api/src/password/dtos/reset-password.dto.ts
+++ node-api/src/password/dtos/reset-password.dto.ts
@@ -1,12 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  MinLength,
-  Validate,
-} from 'class-validator';
-import { IsPasswordMatch } from 'src/common/validators/isPasswordMatch.validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class ResetPasswordDto {
   @IsString()
@@ -17,16 +10,6 @@ export class ResetPasswordDto {
     example: 'newPassword123',
   })
   newPassword: string;
-
-  @IsString()
-  @IsNotEmpty({ message: 'Confirm password should not be empty' })
-  @MinLength(8)
-  @ApiProperty({
-    description: 'Confirmation of the new password',
-    example: 'newPassword123',
-  })
-  @Validate(IsPasswordMatch, ['newPassword'])
-  confirmPassword: string;
 }
 
 export class ResetPasswordRequestDto {
--- node-api/src/password/password.controller.ts
+++ node-api/src/password/password.controller.ts
@@ -51,6 +51,7 @@ export class PasswordController {
     @Body() resetPasswordDto: ResetPasswordDto,
     @Query('token') token?: string,
   ) {
+    console.log('token coming in', token);
     const result = await this.passwordService.resetPassword(
       resetPasswordDto,
       token,
