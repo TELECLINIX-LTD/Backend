0dfd2a251219da9dac855055be55af457cb16150
Dayan Sone <67210349+sonedayan@users.noreply.github.com>
2025-01-15 20:39:05 +0100

    Update main.py

--- python-api/main.py
+++ python-api/main.py
@@ -94,14 +94,13 @@ async def root():
 async def chat(request: Request):
     return templates.TemplateResponse("chat.html", {"request": request})
 
+
 @app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
-    try: 
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await websocket.accept()
+    try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await websocket.send_text(f"Client {client_id} says: {data}")
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} has left the chat")
+        print(f"Client {client_id} disconnected")
